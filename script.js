a7bdbacc95dec1701a21025e605d384fa9c9a80f
Devin <devinpadron28@gmail.com>
2023-04-08 15:58:47 -0400

    Add buttons to webpage. Need to fix broken while loop and display result on top of webpage.

--- script.js
+++ script.js
@@ -1,74 +1,88 @@
-// Simulates games until 5 wins are achieved.
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    const ptsNeeded = 3;
-    while (true) {
-        // Computer Selection
-        function getComputerChoice() {
-            const words = ["r", "p", "s"]; // r(ock), p(aper), s(cissors)
-            randomIndex = Math.floor(Math.random() * words.length);
-            return words[randomIndex];
-        }
-        let computerSelection = getComputerChoice();
-        
-        // Player Selection
-        let playerSelection = prompt("Write r for rock, p for paper, or s for scissors.");
-        playerSelection = playerSelection.toLowerCase();
-        
-        // Error prompt
-        while (playerSelection != "r" && playerSelection != "p" && playerSelection != "s") {
-            playerSelection = prompt("Try again. Type r, p, or s.");
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    // Buttons
+    const buttons =  document.querySelectorAll('.rpsButtons button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+        let playerSelection = button.textContent;
+        game(playerSelection);
+        })
+    })
 
-        let winner = gameRound(playerSelection, computerSelection);
-        if (winner == computerSelection) {
-            computerPoints++;
-        }
-        else if (winner == playerSelection) {
-            playerPoints++;
-        }
-        if (playerPoints == ptsNeeded) {
-            return "You win the game!"
-        }
-        else if (computerPoints == ptsNeeded) {
-            return "You lose the game."
+    // const playButton = document.querySelector('.play');
+    // playButton.addEventListener('click', () => {
+    //     game(playerSelection);
+    // });
+
+    // Result Message
+    function resultMessage(message) {
+        const resultDiv = document.getElementById("result");
+        resultDiv.textContent = message;
+    }
+
+    // Simulates games until "ptsNeeded" is achieved.
+    function game(playerSelection) {
+        let playerPoints = 0;
+        let computerPoints = 0;
+        const ptsNeeded = 1;
+        while (true) {
+            // Computer Selection
+            function getComputerChoice() {
+                const words = ["rock", "paper", "scissors"];
+                randomIndex = Math.floor(Math.random() * words.length);
+                return words[randomIndex];
+            }
+            let computerSelection = getComputerChoice();
+            
+            // Player Selection
+            playerSelection = playerSelection.toLowerCase();
+
+            let winner = gameRound(playerSelection, computerSelection);
+            if (winner == computerSelection) {
+                computerPoints++;
+            }
+            else if (winner == playerSelection) {
+                playerPoints++;
+            }
+            if (playerPoints == ptsNeeded) {
+                resultMessage("You win the game!");
+            }
+            else if (computerPoints == ptsNeeded) {
+                resultMessage("You lose the game.");
+            }
         }
     }
-}
 
-// Simulates one round of the game
-function gameRound(playerSelection, computerSelection) {
+    // Simulates one round of the game
+    function gameRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        console.log("Draw!");
-    } else if (playerSelection == "r") {
-        if (computerSelection == "s") {
-            console.log("Rock beats Scissors. You win!");
-            return playerSelection;
-        }
-        else {
-            console.log("Paper beats Rock. You lose. :-(");
-            return computerSelection;
-        }
-    } else if (playerSelection == "s") {
-        if (computerSelection == "p") {
-            console.log("Scissors beat Paper. You win!");
-            return playerSelection;
-        }
-        else {
-            console.log("Rock beats Scissors. You lose. :-(");
-            return computerSelection;
-        }
-    } else { //if player selects paper
-        if (computerSelection == "r") {
-            console.log("Paper beats Rock. You win!");
-            return playerSelection;
-        }
-        else {
-            console.log("Scissors beat paper. You lose. :-(");
-            return computerSelection;
+        resultMessage("Draw!");
+        } else if (playerSelection == "rock") {
+            if (computerSelection == "scissors") {
+                resultMessage("Rock beats Scissors. You win!");
+                return playerSelection;
+            }
+            else {
+                resultMessage("Rock losses to Paper. You lose. :-(");
+                return computerSelection;
+            }
+        } else if (playerSelection == "scissors") {
+            if (computerSelection == "paper") {
+                resultMessage("Scissors beat Paper. You win!");
+                return playerSelection;
+            }
+            else {
+                resultMessage("Scissors losses to Rock. You lose. :-(");
+                return computerSelection;
+            }
+        } else { //if player selects paper
+            if (computerSelection == "rock") {
+                resultMessage("Paper beats Rock. You win!");
+                return playerSelection;
+            }
+            else {
+                resultMessage("Paper losses to Scissors. You lose. :-(");
+                return computerSelection;
+            }
         }
     }
-}
-
-document.addEventListener("DOMContentLoaded", game);
+})
