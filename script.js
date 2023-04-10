0b2776d4bab408688b69bb824a849d90990f4be9
Devin <devinpadron28@gmail.com>
2023-04-10 13:08:09 -0400

    Make buttons execute game and display result of game in the webpage.

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    // Points
+    const counters = document.querySelectorAll('.score');
+    
+    let playerPoints = 0;
+    let computerPoints = 0;
+
+    function updateCounters () {
+        counters.forEach((counterElem) => {
+            counterElem.textContent = 'Counter ${playerPoints}';
+        })
+    }
+
+    // Computer Selection
+    function getComputerChoice() {
+        const words = ["rock", "paper", "scissors"];
+        randomIndex = Math.floor(Math.random() * words.length);
+        return words[randomIndex];
+    }
+
     // Buttons
     const buttons =  document.querySelectorAll('.rpsButtons button');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
         let playerSelection = button.textContent;
-        game(playerSelection);
+        playerSelection = playerSelection.toLowerCase();
+
+        let computerSelection = getComputerChoice();
+        gameRound(playerSelection, computerSelection);
         })
     })
 
@@ -13,76 +36,44 @@ document.addEventListener('DOMContentLoaded', () => {
     //     game(playerSelection);
     // });
 
+
     // Result Message
     function resultMessage(message) {
         const resultDiv = document.getElementById("result");
         resultDiv.textContent = message;
     }
 
-    // Simulates games until "ptsNeeded" is achieved.
-    function game(playerSelection) {
-        let playerPoints = 0;
-        let computerPoints = 0;
-        const ptsNeeded = 1;
-        while (true) {
-            // Computer Selection
-            function getComputerChoice() {
-                const words = ["rock", "paper", "scissors"];
-                randomIndex = Math.floor(Math.random() * words.length);
-                return words[randomIndex];
-            }
-            let computerSelection = getComputerChoice();
-            
-            // Player Selection
-            playerSelection = playerSelection.toLowerCase();
-
-            let winner = gameRound(playerSelection, computerSelection);
-            if (winner == computerSelection) {
-                computerPoints++;
-            }
-            else if (winner == playerSelection) {
-                playerPoints++;
-            }
-            if (playerPoints == ptsNeeded) {
-                resultMessage("You win the game!");
-            }
-            else if (computerPoints == ptsNeeded) {
-                resultMessage("You lose the game.");
-            }
-        }
-    }
-
     // Simulates one round of the game
     function gameRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        resultMessage("Draw!");
-        } else if (playerSelection == "rock") {
-            if (computerSelection == "scissors") {
-                resultMessage("Rock beats Scissors. You win!");
-                return playerSelection;
-            }
-            else {
-                resultMessage("Rock losses to Paper. You lose. :-(");
-                return computerSelection;
-            }
-        } else if (playerSelection == "scissors") {
-            if (computerSelection == "paper") {
-                resultMessage("Scissors beat Paper. You win!");
-                return playerSelection;
-            }
-            else {
-                resultMessage("Scissors losses to Rock. You lose. :-(");
-                return computerSelection;
-            }
-        } else { //if player selects paper
-            if (computerSelection == "rock") {
-                resultMessage("Paper beats Rock. You win!");
-                return playerSelection;
-            }
-            else {
-                resultMessage("Paper losses to Scissors. You lose. :-(");
-                return computerSelection;
-            }
+        if (playerSelection == computerSelection) {
+            resultMessage("Draw!");
+            } else if (playerSelection == "rock") {
+                if (computerSelection == "scissors") {
+                    resultMessage("Rock beats Scissors. You win!");
+                    playerPoints++;
+                }
+                else {
+                    resultMessage("Rock losses to Paper. You lose. :-(");
+                    computerPoints++;
+                }
+            } else if (playerSelection == "scissors") {
+                if (computerSelection == "paper") {
+                    resultMessage("Scissors beat Paper. You win!");
+                    playerPoints++;
+                }
+                else {
+                    resultMessage("Scissors losses to Rock. You lose. :-(");
+                    computerPoints++;
+                }
+            } else { //if player selects paper
+                if (computerSelection == "rock") {
+                    resultMessage("Paper beats Rock. You win!");
+                    playerPoints++;
+                }
+                else {
+                    resultMessage("Paper losses to Scissors. You lose. :-(");
+                    computerPoints++;
+                }
         }
     }
 })
